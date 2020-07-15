40f169e051d9b39a338f6ef2609d574483bf8f7a
Marcelo Camargo <m.camargo@reply.com>
2020-07-15 19:16:59 -0300

    Criando uma classe e metodo de teste

---
+++ tests/Feature/CustomerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CustomerTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    public function only_logged_in_users_can_see_customers_list()
+    {
+        $response = $this->get('/customers')
+            ->assertRedirect('/login');
+    }
+}
